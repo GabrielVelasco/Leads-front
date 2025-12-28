2577f53efbb654c28efad337ceeff95704cb2bd3
GabrielVelasco <themrgabriel100@gmail.com>
2025-12-28 03:38:02 -0300

    fastAPI '/' endpoint thing... bruh

--- src/api.js
+++ src/api.js
@@ -1,13 +1,13 @@
 const API_BASE = 'https://leads-mng-fadxajdpe2hfajg3.brazilsouth-01.azurewebsites.net';
 
 export async function getLeads() {
-    const res = await fetch(`${API_BASE}/leads`);
+    const res = await fetch(`${API_BASE}/leads/`);
     if (!res.ok) throw new Error('Falha ao buscar leads');
     return res.json();
 }
 
 export async function createLead(data) {
-    const res = await fetch(`${API_BASE}/leads`, {
+    const res = await fetch(`${API_BASE}/leads/`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data),
@@ -20,7 +20,7 @@ export async function createLead(data) {
 }
 
 export async function getLeadById(id) {
-    const res = await fetch(`${API_BASE}/leads/${id}`);
+    const res = await fetch(`${API_BASE}/leads/${id}/`);
     if (!res.ok) {
         const error = await res.json();
         throw error;
